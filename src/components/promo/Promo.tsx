6cb138063217f676aacb15dbab8b2388d6ee8289
Daud <daud_edilbiev@mail.ru>
2025-01-25 19:51:32 +0400

    circle with dotes added

--- src/components/promo/Promo.tsx
+++ src/components/promo/Promo.tsx
@@ -6,13 +6,28 @@ const Promo = () => {
             <div className="wrapper">
                 {/* <h1 className="promo_title">Исторические <br />
                     даты</h1> */}
-                <div className="line middle-vertical"></div>
-                <div className="line middle-horizontal"></div>
                 <div className="main-date-wrapper">
                     <div className="date-start">2015</div>
                     <div className="date-end">2022</div>
                 </div>
+                <div className="line middle-vertical"></div>
+                <div className="line middle-horizontal"></div>
+                <div className="circle">
+                    <div className="one-four">
+                        <div className="one"></div>
+                        <div className="four"></div>
+                    </div>
+                    <div className="two-five">
+                        <div className="two"></div>
+                        <div className="five"></div>
+                    </div>
+                    <div className="three-six">
+                        <div className="three"></div>
+                        <div className="six"></div>
+                    </div>
+                </div>
             </div>
+
         </div>
     )
 }
