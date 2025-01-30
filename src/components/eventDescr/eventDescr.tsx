e077a70b95111a19c536b4d88da03fb779513397
Daud <daud_edilbiev@mail.ru>
2025-01-30 18:32:32 +0400

    changed carousel library

--- src/components/eventDescr/eventDescr.tsx
+++ src/components/eventDescr/eventDescr.tsx
@@ -1,43 +1,60 @@
 import './eventDescr.scss';
 import { IPeriodData } from '../../types/types'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
-import 'swiper.min.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
-const EventDescr = ({ periodData }: { periodData: IPeriodData }) => {
+
+const SimpleSlider = ({ periodData }: { periodData: IPeriodData }) => {
+
+    const settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
 
     return (
         < div className="descr-wrapper" >
-            <button className="swiper-button-prev">Prev</button>
-            <button className="swiper-button-next">Next</button>
-            <Swiper
-                modules={[Navigation]}
-                navigation={{
-                    prevEl: '.swiper-button-prev',
-                    nextEl: '.swiper-button-next',
-                }}
-                spaceBetween={50}
-                slidesPerView={3}
-                onSwiper={(swiper) => console.log(swiper)}
-                onSlideChange={() => console.log('slide change')}
-            >
-
-
-                {
-                    periodData.firstPeriod.map((item) =>
-                    (
-                        <SwiperSlide key={item.id}>
-                            <div className="item">
-                                <div className="year">{item.year} </div>
-                                <div className="text">{item.text}</div>
-                            </div>
-                        </SwiperSlide>
-
-                    ))
-                }
-            </Swiper>
+            <Slider {...settings}>
+                {periodData.firstPeriod.map((item) =>
+                (
+                    <div className="item" key={item.id}>
+                        <div className="year">{item.year} </div>
+                        <div className="text">{item.text}</div>
+                    </div>
+                ))}
+            </Slider >
         </div >
     )
 }
 
-export default EventDescr
+export default SimpleSlider
